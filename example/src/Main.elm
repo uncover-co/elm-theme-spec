10ae9edd66dfc2e93d2f3733337ff7dadc0c8f6c
Georges Boris <georgesboris@gmail.com>
2022-01-10 18:48:29 -0300

    chore: update elm-book examples

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,52 +1,94 @@
 module Main exposing (main)
 
 import ElmBook exposing (Book, book, withChapters)
-import ElmBook.Chapter exposing (chapter, renderComponentList)
-import ThemeSpec
-
-
-baseTheme : ThemeSpec.Theme
-baseTheme =
-    ThemeSpec.lightTheme
-
-
-darkTheme : ThemeSpec.Theme
-darkTheme =
-    ThemeSpec.darkTheme
+import ElmBook.Chapter exposing (chapter, renderComponent)
+import Html exposing (..)
+import ThemeSpec exposing (darkTheme, lightTheme)
 
 
 main : Book ()
 main =
     book "ThemeSpec"
         |> withChapters
-            [ chapter "Sampler"
-                |> renderComponentList
-                    [ ( "Light Theme"
-                      , ThemeSpec.provider
-                            { light = baseTheme
-                            , dark = Nothing
+            [ chapter "Global"
+                |> renderComponent
+                    (div []
+                        [ ThemeSpec.globalProvider lightTheme
+                        , ThemeSpec.sample
+                        ]
+                    )
+            , chapter "Global Dark Mode"
+                |> renderComponent
+                    (div []
+                        [ ThemeSpec.globalProviderWithDarkMode
+                            { light = lightTheme
+                            , dark = darkTheme
+                            , class = Just "elm-book-dark-mode"
+                            }
+                        , ThemeSpec.sample
+                        ]
+                    )
+            , chapter "Global Dark Mode (System)"
+                |> renderComponent
+                    (div []
+                        [ ThemeSpec.globalProviderWithDarkMode
+                            { light = lightTheme
+                            , dark = darkTheme
+                            , class = Nothing
+                            }
+                        , ThemeSpec.sample
+                        ]
+                    )
+            , chapter "Provider"
+                |> renderComponent
+                    (div []
+                        [ ThemeSpec.provider lightTheme
+                            []
+                            [ ThemeSpec.sample
+                            , ThemeSpec.provider darkTheme
+                                []
+                                [ ThemeSpec.sample ]
+                            ]
+                        ]
+                    )
+            , chapter "Provider Dark Mode"
+                |> renderComponent
+                    (div []
+                        [ ThemeSpec.providerWithDarkMode
+                            { light = lightTheme
+                            , dark = darkTheme
+                            , class = Just "elm-book-dark-mode"
                             }
                             []
                             [ ThemeSpec.sample
+                            , ThemeSpec.providerWithDarkMode
+                                { light = darkTheme
+                                , dark = lightTheme
+                                , class = Just "elm-book-dark-mode"
+                                }
+                                []
+                                [ ThemeSpec.sample ]
                             ]
-                      )
-                    , ( "Dark Theme"
-                      , ThemeSpec.provider
-                            { light = darkTheme
-                            , dark = Nothing
+                        ]
+                    )
+            , chapter "Provider Dark Mode (System)"
+                |> renderComponent
+                    (div []
+                        [ ThemeSpec.providerWithDarkMode
+                            { light = lightTheme
+                            , dark = darkTheme
+                            , class = Nothing
                             }
                             []
                             [ ThemeSpec.sample
+                            , ThemeSpec.providerWithDarkMode
+                                { light = darkTheme
+                                , dark = lightTheme
+                                , class = Nothing
+                                }
+                                []
+                                [ ThemeSpec.sample ]
                             ]
-                      )
-                    , ( "Dynamic"
-                      , ThemeSpec.provider
-                            { light = baseTheme
-                            , dark = Just ( Just "elm-book-dark-mode", darkTheme )
-                            }
-                            []
-                            [ ThemeSpec.sample
-                            ]
-                      )
-                    ]
+                        ]
+                    )
             ]
